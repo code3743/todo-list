8f295e3d6de1c6ac85eec1772b3fe35e53d17972
code3743 <jota.lopez@correounivalle.edu.co>
2026-05-13 21:34:21 -0500

    feat: update app ID in config files and add @ionic/cordova-builders dependency

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'dev.jotalopez.todolist',
   appName: 'todo-list',
   webDir: 'www'
 };
